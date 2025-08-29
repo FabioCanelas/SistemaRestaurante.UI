a330d448b8d21946f99c55c72b787b8d23552ef1
Fabio <canelasgodoy30@gmail.com>
2025-08-28 23:55:07 -0400

    Se agrego scripts de la base de datos DDL y DML

---
+++ SistemaRestaurante.UI/ScriptsSQL/DML.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO USUARIO (nombre, username, password, rol) VALUES
+('Administrador', 'admin', 'admin123', 'ADMIN'),
+('Juan Perez', 'juanito', 'juan123', 'RECEPCIONISTA');
+
+INSERT INTO PLATO (nombre, descripcion, precio) VALUES
+('Hamburguesa', 'Hamburguesa con queso y papas', 25.00),
+('Pizza', 'Pizza familiar con extra queso', 50.00),
+('Ensalada', 'Ensalada fresca con aderezo', 15.00),
+('Almuerzo Completo', 'Sopa y segundo', 15.00);
